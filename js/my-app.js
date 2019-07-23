f8fa00b0c3d5a8a23aa1f6c642bb92e9de419797
nitinmak <nitinmakwana539@gmail.com>
2019-07-23 19:25:30 +0530

    fdfd
    fdfd

--- js/my-app.js
+++ js/my-app.js
@@ -40,8 +40,29 @@ document.addEventListener("backbutton", function(e){
  }else{
 
     $$('#bck').trigger("click");
-    if($('.page').data('page') == 'profile'){
-    window.localStorage.setItem("unique",'profile');
+    if($('.page').data('page') != 'home'){
+    $$('#p_bck').trigger("click");
+    
+    }else if($('.page').data('page') != 'home'){
+      $$('#p_bck').trigger("click");
+    }else if($('.page').data('page') != 'lead'){
+      $$('#p_bck').trigger("click");
+    }else if($('.page').data('page') != 'contact_detail'){
+      $$('#p_bck').trigger("click");
+    }else if($('.page').data('page') != 'viewcard'){
+      $$('#p_bck').trigger("click");
+    }else if($('.page').data('page') != 'vcard'){
+      $$('#p_bck').trigger("click");
+    }else if($('.page').data('page') != 'referral'){
+      $$('#p_bck').trigger("click");
+    }else if($('.page').data('page') != 'redeem'){
+      $$('#p_bck').trigger("click");
+    }else if($('.page').data('page') != 'directory'){
+      $$('#p_bck').trigger("click");
+    }else if($('.page').data('page') != 'support'){
+      $$('#p_bck').trigger("click");
+    }else if($('.page').data('page') != 'settings'){
+      $$('#p_bck').trigger("click");
     }else{
     window.localStorage.setItem("unique",0);
     }
